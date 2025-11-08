c10fd9aca8d0c859418a3810c37869039c88c844
Nuno Soares <nuno.soares@mindera.com>
2025-11-08 13:48:39 +0000

    test: add tests to the table page using pom

---
+++ tests/data/table.js
@@ -0,0 +1,26 @@
+export const TABLE_PAGE = {
+  title: /Playground page/,
+  heading: "Test Playground",
+  subheading: "Dynamic Table Instructions",
+  description: "Check out the Harry Potter character table below.",
+};
+
+export const TABLE_COLUMNS = {
+  image: "Image",
+  name: "Name",
+  house: "House",
+  dateOfBirth: "Date of Birth",
+  actor: "Actor",
+};
+
+export const HOUSES = ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"];
+
+export const NAVIGATION = {
+  homeLink: "HOME",
+  loginLink: "LOGIN",
+  formLink: "FORM",
+  tableLink: "TABLE",
+  tasksLink: "TASKS",
+  storeLink: "STORE",
+  aboutLink: "ABOUT",
+};
